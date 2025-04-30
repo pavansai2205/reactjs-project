b08ffb4ce79cb5f1691192e9d0bec9129fad0abc
Pavansai2205 <saipavan931@gmail.com>
2025-04-30 19:28:40 +0530

    checking webhook

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -20,7 +20,7 @@ function Navbar() {
         <div className="menu-line menu-line-2"></div>
         <div className="menu-line menu-line-3"></div>
       </div>
-      <h1>sai studio</h1>
+      <h1>sai Ganesh studio</h1>
       <ul>
         {NavbarItems.map((item, index) => {
           return (
